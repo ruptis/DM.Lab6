bbf862bc266e3bba3b053ba257234dc0bc3f85b7
ruptis <90418759+ruptis@users.noreply.github.com>
2023-10-14 15:29:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import argparse
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
+from scipy.cluster.hierarchy import linkage
+from scipy.spatial.distance import squareform
+
+DEFAULT_MATRIX = [
+    [0., 5., 0.5, 2.],
+    [5., 0., 1., 0.6],
+    [0.5, 1., 0., 2.5],
+    [2., 0.6, 2.5, 0.],
+]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--size', type=int, help='Generate random matrix of given size')
+    parser.add_argument('--method', type=str, default='min', help='Method of distance calculation (min, max)')
+    args = parser.parse_args()
+
+    if args.size is not None:
+        matrix = np.random.random((args.size, args.size))
+        matrix = (matrix + matrix.T) / 2
+        np.fill_diagonal(matrix, 0)
+    else:
+        matrix = DEFAULT_MATRIX
+
+    print_matrix(matrix)
+    linkage_matrix = linkage(squareform(matrix), method='single' if args.method == 'min' else 'complete')
+    dendrogram(linkage_matrix, labels=np.array(['x%d' % i for i in range(1, len(matrix) + 1)]))
+    plt.show()
+
+
+def print_matrix(matrix):
+    print('\t' + '\t'.join(['x%d' % i for i in range(1, len(matrix) + 1)]))
+    for i, row in enumerate(matrix):
+        print('x%d\t' % (i + 1) + '\t'.join(['%.2f' % x for x in row]))
+
+
+if __name__ == '__main__':
+    main()
